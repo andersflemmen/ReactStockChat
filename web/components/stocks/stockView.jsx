9e0c281c753ada5e2f6385ccc83c89c593a8e7d5
Anders Kvalvaag <anderskvalvaag@gmail.com>
2015-11-05 13:55:05 +0100

    Added stock view and updated the other components to use it

---
+++ web/components/stocks/stockView.jsx
@@ -0,0 +1,9 @@
+var StockView = React.createClass({
+        render: function() {
+          return (
+            <div className="StockView">
+              <StockSearch/>
+            </div>
+          );
+        }
+});
