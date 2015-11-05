e3c0da29a5e36dd2f5d4d3a17b9d11d37c1f6dfc
Leif Arne <leif_xx@hotmail.com>
2015-11-05 13:40:38 +0100

    Merge origin/master
    Conflicts:
    	src/java/no/hib/mod250/rest/MessageFacadeREST.java
    	src/java/no/hib/mod250/stocks/Stock.java
    	web/components/stocks/stock.jsx
    	web/components/stocks/stockbox.jsx
    	web/stock.html

---
+++ web/components/chat/messagebox.jsx
@@ -0,0 +1,51 @@
+   var CommentBox = React.createClass({
+           
+           loadCommentsFromServer: function(){
+             $.ajax({
+                url: 'REST/messages/last',
+                dataType: 'json',
+                cache: false,
+                success: function(data){
+                    this.setState({data: data});
+                }.bind(this),
+                error: function(xhr, status, err){
+                    console.error(this.props.url, status, err.toString());
+                }.bind(this)
+             });  
+         },
+         handleCommentSubmit: function(comment){
+             var comments = this.state.data;
+             var newComments = comments.concat([comment]);
+             this.setState({data: newComments});
+             var data = JSON.stringify(comment);
+           $.ajax({
+               url: 'REST/messages/new/',
+               contentType: 'application/json; charset=utf-8',
+               dataType: 'json',
+               type: 'POST',
+               data: data,
+               success: function(){
+             
+               }.bind(this),
+               error: function(xhr, status, err){
+                   console.error(this.props.url, status, err.toString());
+               }.bind(this)
+           }); 
+         },
+           getInitialState: function() {
+             return {data: []};  
+           },
+           componentDidMount: function(){
+            this.loadCommentsFromServer();
+            setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+           },
+          render: function() {
+              return(
+                <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+                </div>
+             );
+          } 
+       });
