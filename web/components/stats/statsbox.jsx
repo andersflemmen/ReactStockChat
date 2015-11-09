153d4f2b1fdb4d44a014a31a3b98ee751033c259
Anders Kvalvaag <anderskvalvaag@gmail.com>
2015-11-09 11:45:35 +0100

    Styled the stock page

---
+++ web/components/stats/statsbox.jsx
@@ -0,0 +1,59 @@
+var StatsBox = React.createClass({
+    getStats: function() {
+              $.ajax({
+                url: this.props.url,
+                dataType: 'json',
+                cache: false,
+                success: function(data) {
+                    this.setState({
+                      stats: data,
+                      top10: data.top_ten_commented
+                    });
+                }.bind(this),
+                error: function(xhr, status, err) {
+                  console.error(this.props.url, status, err.toString());
+                }.bind(this)
+              });
+    },
+
+    componentDidMount: function() {
+        setInterval(this.getStats, this.props.pollInterval);
+        this.getStats();
+    },
+
+    getInitialState: function() {
+        return {stats: { top_ten_commented: [] }, top10: []};
+    },  
+
+    render: function() {
+
+        var top10 = this.state.stats.top_ten_commented.map(function(stock) {
+                        return (<li>{stock.symbol} ({stock.count})</li>);
+                    }, this);
+
+        return (
+            <div className="panel panel-info pull-left stockPanel">
+                <div className="panel-heading">
+                    <h3 className="panel-title">
+                        <b>Statinator</b>
+                    </h3>
+                </div>
+                <br />
+                <div>
+                    <ul>
+                        <li>Number of comments: {this.state.stats.num_comments}</li>
+                        <li>Number of comments today: {this.state.stats.num_comments_today}</li>
+                        <li>Number of chat messages: {this.state.stats.num_messages}</li>
+                        <li>Number of chat messages today: {this.state.stats.num_messages_today}</li>
+                        <li>
+                            Most popular stocks (comments):
+                            <ol>
+                                {top10}
+                            </ol>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+});
