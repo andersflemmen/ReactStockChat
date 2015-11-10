f8bd8d1ff5a37583298ba706a1879779b29b099b
exoen <andy_lill@hotmail.com>
2015-11-10 11:49:48 +0100

    endret navbar og byttet index

--- web/components/navbar/navbar.jsx
+++ web/components/navbar/navbar.jsx
@@ -13,16 +13,9 @@ var MyNavbarInstance = React.createClass ({
       <Nav right eventKey={0}>
         <NavItem eventKey={1} href="index.html">Home</NavItem>
         <NavItem eventKey={2} href="currencies.html">Currencies</NavItem>
-        <NavItem eventKey={3} href="stock.html">Stocks</NavItem>
         <NavItem eventKey={3} href="commenttest.html">Comment test</NavItem>
         <NavItem eventKey={3} href="chat.html">Chat</NavItem>
-        <NavItem eventKey={3} href="currencies-vanillajs.html">currencies-vanillajs</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="collapsible-navbar-dropdown">
-          <MenuItem eventKey="1">index</MenuItem>
-          <MenuItem eventKey="2" >Currencies</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="3" >Currencies again</MenuItem>
-        </NavDropdown>
+        <NavItem eventKey={3} href="currencies-vanillajs.html">Currencies-vanillajs</NavItem>
       </Nav>
     </Navbar>
     </div>
