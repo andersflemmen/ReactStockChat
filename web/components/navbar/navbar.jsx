40031bcad415f62074319dc9ee866d9a0d7868fe
exoen <andy_lill@hotmail.com>
2015-11-11 15:45:32 +0100

    oppdatert navbar

--- web/components/navbar/navbar.jsx
+++ web/components/navbar/navbar.jsx
@@ -13,13 +13,14 @@ var MyNavbarInstance = React.createClass ({
       <Nav right eventKey={0}>
         <NavItem eventKey={1} href="index.html">Home</NavItem>
         <NavItem eventKey={2} href="currencies.html">Currencies</NavItem>
-        <NavItem eventKey={3} href="currencies-vanillajs.html">Currencies vanillajs</NavItem>
-        <NavItem eventKey={3} href="currencies-smartjs.html">Currencies SmartJS</NavItem>
-        <NavItem eventKey={3} href="vanilla-vs-react.html">Vanilla vs React</NavItem>
+        <NavItem eventKey={3} href="vanilla-vs-react.html">JS vs React</NavItem>
         <NavDropdown eventKey={3} title="Test Cases" id="basic-nav-dropdown">
         <MenuItem eventKey="1" href="brutejs-currency.html">BruteJS currency</MenuItem>
         <MenuItem eventKey="2" href="smartjs-currency.html">SmartJS currency</MenuItem>
         <MenuItem eventKey="3" href="reactjs-currency.html">React currency</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey="1" href="currencies-vanillajs.html">Currencies VanillaJS</MenuItem>
+        <MenuItem eventKey="2" href="currencies-smartjs.html">Currencies SmartJS</MenuItem>
         <MenuItem divider />
         </NavDropdown>
       </Nav>
